31b337fa165ab71283bfa9fe3942560e2e8697e4
vdinovi <vito.dinovi@gmail.com>
2016-10-12 18:16:01 -0700

    updates

--- server.js
+++ server.js
@@ -3,18 +3,17 @@ var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var crypto = require('crypto');
 
+// Custom modules
 var db = require('./bin/db.js');
 
+// Setup and config server
 var server = express();
-
 server.use(bodyParser.json());
 server.use(express.static('public'));
+var db = require('./bin/db.js');
+var dbconn = new db();
 
-var DBconn = function() {
-    this.conn = null;
-};
 
-var conn = new db();
-conn.query('test');
+
 
 server.listen(3030);
