1529c1386556c2dbce01f1445c24658a4e7bc265
vdinovi <vito.dinovi@gmail.com>
2016-10-24 00:33:53 -0700

    chat ui

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
     // Connect
     var socket = io('http://localhost:3030');
+
+    // Handle message
     socket.on('message', function(msg) {
         console.log(msg.data);
         var cur = $("#chat-window-box");
-        cur.val(cur.val() + '\n' + msg.data);
+        cur.val(cur.val() + '\nUser: ' + msg.data);
     });
-    
+
+    // Send message
     $("#input-form").submit(function(e) {
         e.preventDefault();
         var box = $("#input-text");
