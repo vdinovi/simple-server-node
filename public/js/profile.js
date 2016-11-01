ea5f957b91e82111ffabab941827bb33f720b223
vdinovi <vito.dinovi@gmail.com>
2016-11-01 11:31:33 -0700

    aws

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,4 +1,4 @@
-const host = "http://http://simpleserver.bfmpgunfdg.us-west-1.elasticbeanstalk.com/";
+const host = "http://simpleserver.bfmpgunfdg.us-west-1.elasticbeanstalk.com/";
 
 function renderProfile() {
     $.ajax({
